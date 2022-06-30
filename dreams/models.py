d7305022713e50a4f9e8e455103e315824d98b05
Jyuri <jeon1310@gmail.com>
2022-07-01 05:02:27 +0900

    view, new html 완료

--- dreams/models.py
+++ dreams/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+from account.models import CustomUser
+from dreamColor import settings
+
+
+class DreamModel(models):
+    title = models.CharField(verbose_name='제목' ,max_length=30),
+    date_dream = models.DateField(verbose_name='꿈꾼날짜'),
+    created = models.DateTimeField(verbose_name='생성시각',auto_now_add=True),
+    keyword = models.CharField(verbose_name='키워드들', max_length=40),
+    read_cnt = models.IntegerField(verbose_name='읽음개수',default=0),
+    color = models.CharField(verbose_name='꿈색깔',max_length=10),
+    contents = models.TextField(verbose_name='내용'),
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- dreams/views.py
+++ dreams/views.py
@@ -1,8 +1,15 @@
+import datetime
+
 from django.shortcuts import render
 
 # Create your views here.
 def createDream(request):
-    return render('dreams/new.html',{{}})
+    return render(request,'dreams/new.html')
+
+def viewDream(request):
+    dream = {'title':'추락하는 꿈', 'date_dream':datetime.date,'keyword':['나라','사랑','친구'],'bg':'#fffff','contents':'내용ㅇㅇㅇ'
+             ,'user':'ㅇㄹㅇㄴ', 'read':21}
+    return render(request,'dreams/view.html',{'dream':dream})
 
 def mainPage(request):
     return render(request, 'dreams/main.html')
