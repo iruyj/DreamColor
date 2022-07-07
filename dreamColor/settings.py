921c00fe4a64adb4c259979e1edcdcf2ef7c03f7
w2009 <w2009@e-mirim.hs.kr>
2022-07-07 22:05:21 +0900

    add site_id

--- dreamColor/settings.py
+++ dreamColor/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
-
+SITE_ID=1
 
 # Application definition
 
