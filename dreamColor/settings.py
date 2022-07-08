b394f2b10c810c37d3f3d5914fb7cedd013e2a3c
Jyuri <jeon1310@gmail.com>
2022-07-08 09:51:06 +0900

    static setting

--- dreamColor/settings.py
+++ dreamColor/settings.py
@@ -27,6 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DJANGO_DEBUG') == 'True'
+# DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
 
